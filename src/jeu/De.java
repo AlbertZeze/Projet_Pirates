c3d74d26eda9fd86ae9b98fd5eefc48f10ab9ecc
AlbertZeze <albertzezeguilavogui@gmail.com>
2026-03-03 13:43:11 +0100

    Developpement de la classe de

--- src/jeu/De.java
+++ src/jeu/De.java
@@ -1,5 +1,18 @@
 package jeu;
+import java.util.Random;//la librairie permettant de générer aleatoirement les nombres de 1 à 6
 
 public class De {
+//Création des attributs
+	private Random random; //pour generer des nombres de 1 à 6
 
+	//Création du constructeur
+	public De() {
+		random = new Random();
+		
+	}
+	
+	//Création de la méthode lancerDe()
+	public int lancerDe() {
+		return random.nextInt(6) + 1;
+	}
 }
