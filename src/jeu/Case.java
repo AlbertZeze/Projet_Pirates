76145287d74afcce64e4abe8630f02063a7e8066
AlbertZeze <albertzezeguilavogui@gmail.com>
2026-03-09 20:51:09 +0100

    fin du developpement de la case RouletteRusse

--- src/jeu/Case.java
+++ src/jeu/Case.java
@@ -1,5 +1,18 @@
 package jeu;
 
 public abstract class Case {
+	//Déclaration des attributs
+	protected int numeroCase;//puisque la classe est abstraite; les attributs sont de types protected
+	
+	//création du constructeur
+	public Case(int numeroCase) {
+		this.numeroCase=numeroCase;
+	}
 
+	//Le getteur
+	public int getNumeroCase() {
+		return numeroCase;
+	}
+
+	public abstract void declencherAction(Joueur joueur1, Joueur joueur2);
 }
